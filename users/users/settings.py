e42a5b1365cc1c92fb3f2d6cdc030db6e9c54a6a
Roman N <roman.olbgroup@gmail.com>
2024-07-10 11:49:47 +0300

    set auth tokens lifetime to 3 days

--- users/users/settings.py
+++ users/users/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -49,6 +50,10 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
+}
+
+SIMPLE_JWT = {
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=3)
 }
 
 AUTH_USER_MODEL = 'users_api.CustomUser'
