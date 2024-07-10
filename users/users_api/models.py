ecee29f8f6311df3191f4ff6b159d5d5594ba368
Roman N <roman.olbgroup@gmail.com>
2024-07-10 10:01:46 +0300

    create user model

--- users/users_api/models.py
+++ users/users_api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    desiredSalary = models.IntegerField()
+    grade = models.CharField(max_length=100)
+    exp = models.IntegerField()
+    profilePhoto = models.CharField(max_length=100)
+    technologies = models.JSONField()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'name', 'country', 'desiredSalary', 'grade', 'exp', 'technologies']
